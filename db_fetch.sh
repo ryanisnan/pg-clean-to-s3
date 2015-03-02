387bbae0ba52d5cb44635958977d7c9e1d29659e
Ryan West <ryanisnan@gmail.com>
2015-03-02 15:10:17 -0800

    small update to fetch params

--- db_fetch.sh
+++ db_fetch.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 echo "Downloading a dump of the database"
-pg_dump -Fc -h $REMOTE_DB_HOST -p $REMOTE_DB_PORT -d $REMOTE_DB_NAME -U $REMOTE_DB_USER -W > ../latest.dump
+pg_dump -h $REMOTE_DB_HOST -p $REMOTE_DB_PORT -d $REMOTE_DB_NAME -U $REMOTE_DB_USER -W -Fc > ../latest.dump
