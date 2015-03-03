5d97c744fb90fa94823295f48d48764a55bea2ab
Ryan West <ryanisnan@gmail.com>
2015-03-02 17:29:44 -0800

    add if/else conditions to main script

--- run.sh
+++ run.sh
@@ -1,10 +1,35 @@
 chmod +x ./src/*.sh
 
 ./src/db_download.sh
+if [ $? -eq 0 ]
+    then
+        exit
+fi
+
 ./src/db_restore.sh
+if [ $? -eq 0 ]
+    then
+        exit
+fi
+
 ./src/db_prune.sh
+if [ $? -eq 0 ]
+    then
+        exit
+fi
+
 ./src/db_dump.sh
+if [ $? -eq 0 ]
+    then
+        exit
+fi
+
 ./src/db_upload.sh
+if [ $? -eq 0 ]
+    then
+        exit
+fi
+
 ./src/cleanup.sh
 
 echo "Done."
