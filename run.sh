382a5958a6fdd59e2731bdd0f95ff21c2b8fa7f6
Ryan West <ryanisnan@gmail.com>
2015-03-02 17:31:25 -0800

    update comparison operators

--- run.sh
+++ run.sh
@@ -1,31 +1,31 @@
 chmod +x ./src/*.sh
 
 ./src/db_download.sh
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
     then
         exit
 fi
 
 ./src/db_restore.sh
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
     then
         exit
 fi
 
 ./src/db_prune.sh
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
     then
         exit
 fi
 
 ./src/db_dump.sh
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
     then
         exit
 fi
 
 ./src/db_upload.sh
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
     then
         exit
 fi
