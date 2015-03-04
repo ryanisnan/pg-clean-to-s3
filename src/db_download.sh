331444fb5864761977378fff606a8be8f9ad7b95
Ryan West <ryanisnan@gmail.com>
2015-03-04 10:00:56 -0800

    pass in remote db name as parameter to pg_dump and not as option value

--- src/db_download.sh
+++ src/db_download.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "Downloading a dump of the database..."
-pg_dump -h $REMOTE_DB_HOST -p $REMOTE_DB_PORT -d $REMOTE_DB_NAME -U $REMOTE_DB_USER -W -Fc > ../latest.dump
+pg_dump -h $REMOTE_DB_HOST -p $REMOTE_DB_PORT -U $REMOTE_DB_USER -W -Fc $REMOTE_DB_NAME > ../latest.dump
 
 if [ $? -ne 0 ]
     then
