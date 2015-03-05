418820d2518eebd9cfd833a8cd994ec13b8cc4cb
Ryan West <ryanisnan@gmail.com>
2015-03-04 17:48:40 -0800

    make uploaded DB have a timestamp with the day

--- src/db_upload.sh
+++ src/db_upload.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
 echo "Preparing local database as latest_new.dump"
-pg_dump -U $LOCAL_DB_USER -Fc $LOCAL_DB_NAME > ../latest_new.dump
+
+output_file=$(date +%Y%m%d)_db.dump
+
+pg_dump -U $LOCAL_DB_USER -Fc $LOCAL_DB_NAME > ../$output_file
 
 if [ $? -ne 0 ]
     then
@@ -9,4 +12,4 @@ if [ $? -ne 0 ]
 fi
 
 echo "Uploading file to S3..."
-aws s3 mv ../latest.dump s3://$AWS_S3_BUCKET_NAME/latest.dump
+aws s3 mv ../latest.dump s3://$AWS_S3_BUCKET_NAME/$output_file
